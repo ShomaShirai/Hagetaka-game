f9b41fff2e8d895a07f63e6b496753eec55b5d2f
ShomaShirai <takosu9314@gmail.com>
2025-06-30 19:11:20 +0900

    いらないコードの削除

--- src/firebase/db_handler.ts
+++ src/firebase/db_handler.ts
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDcASrcVKgu8gkCF2IIGlXejGq-aW-Z_jo",
-  authDomain: "hagetaka-game.firebaseapp.com",
-  projectId: "hagetaka-game",
-  storageBucket: "hagetaka-game.firebasestorage.app",
-  messagingSenderId: "912631818840",
-  appId: "1:912631818840:web:1f8a98d51919960535f82d",
-  measurementId: "G-TDP9S13PZQ"
+  
 };
 
 // Initialize Firebase
